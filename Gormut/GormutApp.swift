bc19c6f15a8dc0cf098f39619baa7ee7b2ea9375
Afnan Alhabi <afnan.m.alhabi@gmail.com>
2022-12-20 19:20:31 +0300

    Initial Commit

---
+++ Gormut/GormutApp.swift
@@ -0,0 +1,17 @@
+//
+//  GormutApp.swift
+//  Gormut
+//
+//  Created by Afnan on 26/05/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct GormutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
